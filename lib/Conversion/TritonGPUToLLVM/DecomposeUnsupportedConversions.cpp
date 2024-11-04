fd949b6118949c53ffdc23e0e7a223c9ebf330a5
Alexander Efimov <efimov.alexander@gmail.com>
2024-11-04 15:33:46 +0000

    fix lit tests

--- lib/Conversion/TritonGPUToLLVM/DecomposeUnsupportedConversions.cpp
+++ lib/Conversion/TritonGPUToLLVM/DecomposeUnsupportedConversions.cpp
@@ -83,7 +83,7 @@ void decomposeBlockedToDotLayoutConversion(ModuleOp module) {
     OpBuilder builder(cvtOp);
     auto srcType = cast<RankedTensorType>(cvtOp.getSrc().getType());
     auto dstType = cast<RankedTensorType>(cvtOp.getType());
-    if (isMoeLDSBypass() && !cvtNeedsSharedMemory(srcType, dstType))
+    if (!cvtNeedsSharedMemory(srcType, dstType))
       return;
     auto srcBlocked =
         dyn_cast<triton::gpu::BlockedEncodingAttr>(srcType.getEncoding());
